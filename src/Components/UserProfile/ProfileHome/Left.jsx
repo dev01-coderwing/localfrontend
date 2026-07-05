2eb291ea61a4aecee4c47d16dff00e2392c7f9a5
Novameon <novameon@gmail.com>
2026-07-05 03:27:24 +0200

    Refactor icon display logic in UserProfile component

--- src/Components/UserProfile/ProfileHome/Left.jsx
+++ src/Components/UserProfile/ProfileHome/Left.jsx
@@ -124,11 +124,13 @@ function Item({ item = {}, tick }) {
       border-gray-100 shadow-sm"
     >
       <div className="flex items-center gap-3">
-        <div className="w-9 h-9 rounded-full flex items-center justify-center">
-          {item?.img ? (
-            <img src={item.img} alt={item?.title} className="w-5 h-5" />
-          ) : (
-            <Star size={18} className="text-yellow-500" />
+        <div className="w-9 h-9 rounded-full flex items-center justify-center text-lg">
+  {item?.icon ? (
+    <span>{item.icon}</span>
+  ) : item?.img ? (
+    <img src={item.img} alt={item?.title} className="w-5 h-5" />
+  ) : (
+    <Star size={18} className="text-yellow-500" />
           )}
         </div>
 
