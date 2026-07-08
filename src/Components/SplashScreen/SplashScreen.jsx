8dfe02bf1af7a0075b20326a21fb5df68d10f7b9
Novameon <novameon@gmail.com>
2026-07-08 02:23:17 +0200

    Increase logo width from 120px to 130px

--- src/Components/SplashScreen/SplashScreen.jsx
+++ src/Components/SplashScreen/SplashScreen.jsx
@@ -47,7 +47,7 @@ export default function SplashScreen() {
         src="/Image/iameetyou.png" 
         alt="IAMeetYou Logo"
         style={{
-          width: '120px',
+          width: '130px',
           height: 'auto',
           animation: 'splashLogoIn 0.9s cubic-bezier(0.22, 1, 0.36, 1) forwards',
         }}
